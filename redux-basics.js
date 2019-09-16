fa48e14e1bbc98adb81cd212db8deda2db20c43c
Inna Mon <inna.mon@gmail.com>
2019-09-16 10:18:45 -0500

    added logic in reducer in order to update state

--- redux-basics.js
+++ redux-basics.js
@@ -7,6 +7,18 @@ const initialState = {
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter++
+        }
+    }
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
     return state;
 };
 
@@ -15,5 +27,9 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 //Dispatching Action
+
+store.dispatch({type: 'INC_COUNTER'}); //incremenets by one
+store.dispatch({type: 'ADD_COUNTER', value: 10}); //adds specific number to counter, value needs to be passed down along with type
+console.log(store.getState());
 
 //Subscription
